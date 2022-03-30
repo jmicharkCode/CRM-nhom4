b8c2b286979d9cf31522c77bda0e7d73411ec275
jmicharkCode <88967855+jmicharkCode@users.noreply.github.com>
2022-03-30 08:08:32 +0700

    change sql

--- crm-app-main/java16-crm-nhom-4/crm.sql
+++ crm-app-main/java16-crm-nhom-4/crm.sql
@@ -1,89 +1,90 @@
-create database if not exists `crm`;
+create database crm;
+use crm;
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  `role_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+INSERT INTO `user` VALUES (1,'hoan26@gmail.com','123','Nguyễn Việt Hoàn','Ninh Bình','0966712112',1),(2,'haha@gmail.com','12345','Phạm Văn Hiệp','Ninh Bình','09888123123',2);
+DROP TABLE IF EXISTS `task`;
 
-use `crm`;
-
-create table if not exists `role` (
-	id 			int,
-    name 		varchar(50) not null,
-    description varchar(500),
-    primary key(id)
+CREATE TABLE `task` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `project_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `status_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
-create table if not exists `status` (
-	id 			int,
-    name	 	varchar(50) not null,
-    description varchar(500),
-    primary key(id)
+DROP TABLE IF EXISTS `status`;
+CREATE TABLE `status` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
-create table if not exists `user`(
-	id 			int,
-    email 		varchar(50) not null unique,
-    password 	varchar(255) not null,
-    fullname 	varchar(255),
-    address 	varchar(500),
-    phone 		varchar(36),
-    role_id 	int not null,
-    primary key (id)
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
-create table if not exists `task`(
-	id			int,
-    name		varchar(100) not null,
-    description varchar(500),
-    start_date	date default(NOW()),
-    due_date	date,
-    assignee	int,
-    project_id	int,
-    status_id	int,
-    primary key (id)
+DROP TABLE IF EXISTS `project_user`;
+
+CREATE TABLE `project_user` (
+  `project_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `join_date` date DEFAULT NULL,
+  `role_description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`project_id`,`user_id`)
+ 
 );
 
-create table if not exists `project`(
-	id			int,
-    name		varchar(100) not null,
-    description varchar(500),
-    start_date	date,
-    end_date	date,
-    create_user_id int,
-    primary key (id)
-);
+DROP TABLE IF EXISTS `project`;
 
-create table if not exists `project_user`(
-	project_id	int,
-    user_id		int,
-    join_date	date default(NOW()),
-    role_description varchar(500),
-    primary key (project_id, user_id)
-);
-
--- add foreign key
-alter table `user`
-	add constraint `fk_user_role`
-		foreign key (role_id) references `role`(id);
+CREATE TABLE `project` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `owner` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+  
+) ;
+INSERT INTO `role` VALUES (1,'ADMIN','Quản trị hệ thống'),(2,'LEADER','Quản lý dự án'),(3,'MEMBER','Thành viên');
+ALTER TABLE user
+	ADD CONSTRAINT FK_USER_ROLE
+		FOREIGN KEY (role_id) REFERENCES role(id); 
         
-alter table `task`
-	add constraint `fk_task_user`
-		foreign key (assignee) references `user`(id);
-alter table `task`
-	add constraint `fk_task_project`
-		foreign key (project_id) references `project`(id);
-alter table `task`
-	add constraint `fk_task_status`
-		foreign key (status_id) references `status`(id);
-        
-alter table `project`
-	add constraint `fk_project_user`
-		foreign key (create_user_id) references `user`(id);
-        
-alter table `project_user`
-	add constraint `fk_project_user_to_user`
-		foreign key (user_id) references `user`(id);
-alter table `project_user`
-	add constraint `fk_project_user_to_project`
-		foreign key (project_id) references `project`(id);
-INSERT INTO role(id, name, description) VALUES(1, 'ADMIN', 'Quản trị hệ thống');
-INSERT INTO role(id, name, description) VALUES(2, 'LEADER', 'Quản lý dự án');
-INSERT INTO role(id, name, description) VALUES(3, 'MEMBER', 'Thành viên');
-INSERT INTO user(id, email, password, name, address, phone, role_id) VALUES('hoan26@gmail.com', '123','Nguyễn Việt Hoàn','Ninh Bình', '0966712112',1);
-INSERT INTO user(id, email, password, name, address, phone, role_id) VALUES('hiep261@gmail.com', '12345','Phạm Bá Hiệp','Ninh Bình', '0966712334',2);
+ALTER TABLE project
+	ADD CONSTRAINT FK_PROJECT_USER_OWNER
+		FOREIGN KEY (owner) REFERENCES user(id); 
+	
+ALTER TABLE project_user
+	ADD CONSTRAINT FK_PROJECT_USER_PROJECT_USER_LIST
+		FOREIGN KEY (project_id) REFERENCES project(id),
+	ADD CONSTRAINT FK_PROJECT_USER_USER_JOIN_LIST
+		FOREIGN KEY (user_id) REFERENCES user(id);
+
+ALTER TABLE task
+	ADD CONSTRAINT FK_TASK_USER
+		FOREIGN KEY (user_id) REFERENCES user(id),
+	ADD CONSTRAINT FK_TASK_PROJECT
+		FOREIGN KEY (project_id) REFERENCES project(id),
+	ADD CONSTRAINT FK_TASK_STATUS
+		FOREIGN KEY (status_id) REFERENCES status(id);
